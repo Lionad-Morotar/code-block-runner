4ec16a9f0f1492abf65589ad7223562367db2810
Lionad <tangnad@qq.com>
2020-07-07 21:59:41 +0800

    stash: demo for vue-server-renderer

---
+++ server.js
@@ -0,0 +1,38 @@
+const Vue = require('vue')
+const server = require('express')()
+
+const template = require('fs').readFileSync('./public/server.template.html', 'utf-8')
+
+const renderer = require('vue-server-renderer').createRenderer({
+  template,
+})
+
+const context = {
+    title: 'vue ssr',
+    metas: `
+        <meta name="keyword" content="vue,ssr">
+        <meta name="description" content="vue srr demo">
+    `,
+}
+
+server.get('/index', (req, res) => {
+  res.setHeader("Content-type", "text/html;charset=UTF-8"); 
+
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>访问的 URL 是： {{ url }}</div>`,
+  })
+
+  renderer.renderToString(app, context, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      console.error(err)
+      return
+    }
+    res.end(html)
+  })
+})
+
+server.listen(8888)
